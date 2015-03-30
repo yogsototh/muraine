7b43f46b2a12eb00d95a7b44161c32cf2d860793
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2015-03-30 19:33:45 +0200

    github events without authentication

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+-- import           Data.Conduit
+import           Network.HTTP.Conduit
+import           Network.HTTP.Types.Header (Header)
+import qualified Data.ByteString.Lazy.Char8 as LZ
+import qualified Data.ByteString.Char8 as B
+import Data.CaseInsensitive (original)
+import Data.Monoid ((<>))
+
+simpleHTTPWithUserAgent :: String -> IO (Response LZ.ByteString)
+simpleHTTPWithUserAgent url = do
+    r <- parseUrl url
+    let request = r {requestHeaders = [("User-Agent","HTTP-Conduit")]}
+    withManager $ \manager -> httpLbs request manager
+
+showHeader :: Header -> IO ()
+showHeader (name, value) = B.putStrLn $ original name <> ": " <> value
+
 main :: IO ()
-main = putStrLn "Hello Muraine!"
+main = do
+    response <- simpleHTTPWithUserAgent "https://api.github.com/events"
+    LZ.putStrLn (responseBody response)
+    mapM_ showHeader (responseHeaders response)

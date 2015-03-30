b580ec2152af2f467c9d3678838768f59d040222
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2015-03-30 20:06:54 +0200

    basic auth

--- src/Main.hs
+++ src/Main.hs
@@ -8,18 +8,33 @@ import qualified Data.ByteString.Lazy.Char8 as LZ
 import qualified Data.ByteString.Char8 as B
 import Data.CaseInsensitive (original)
 import Data.Monoid ((<>))
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
-simpleHTTPWithUserAgent :: String -> IO (Response LZ.ByteString)
-simpleHTTPWithUserAgent url = do
+simpleHTTPWithUserAgent :: String -> String -> String -> IO (Response LZ.ByteString)
+simpleHTTPWithUserAgent url user pass = do
     r <- parseUrl url
     let request = r {requestHeaders = [("User-Agent","HTTP-Conduit")]}
-    withManager $ \manager -> httpLbs request manager
+        requestWithAuth = applyBasicAuth (B.pack user) (B.pack pass) request
+    withManager $ \manager -> httpLbs requestWithAuth manager
 
 showHeader :: Header -> IO ()
 showHeader (name, value) = B.putStrLn $ original name <> ": " <> value
 
+showHelpAndExit :: IO ()
+showHelpAndExit = do
+    putStrLn "provide your github username and password please"
+    exitFailure
+
 main :: IO ()
 main = do
-    response <- simpleHTTPWithUserAgent "https://api.github.com/events"
+    args <- getArgs
+    case args of
+         [user,pass] -> continueWithUserAndPass user pass
+         _ -> showHelpAndExit
+
+continueWithUserAndPass :: String -> String -> IO ()
+continueWithUserAndPass user pass = do
+    response <- simpleHTTPWithUserAgent "https://api.github.com/events" user pass
     LZ.putStrLn (responseBody response)
     mapM_ showHeader (responseHeaders response)

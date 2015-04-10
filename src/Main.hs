680f29fb6bca4e70318d6ca71c9d56742d73f7b6
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2015-04-10 23:02:10 +0200

    some refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,7 @@ import Data.Text (Text)
 import qualified Data.Text as T
 
 import           Network.HTTP.Conduit
-import           Network.HTTP.Types.Header (Header,RequestHeaders)
+import           Network.HTTP.Types.Header (Header,RequestHeaders,ResponseHeaders)
 import           Network.HTTP.Types.Status (statusIsSuccessful,notModified304)
 import qualified Data.ByteString.Lazy.Char8 as LZ
 import qualified Data.ByteString.Char8 as B
@@ -23,38 +23,58 @@ import Data.Time.Clock.POSIX (getPOSIXTime,utcTimeToPOSIXSeconds)
 import System.Locale (defaultTimeLocale)
 import System.CPUTime (getCPUTime)
 
-httpCall :: String -> RequestHeaders -> IO (Response LZ.ByteString)
-httpCall url headers = do
-    r <- parseUrl url
-    let request = r { requestHeaders = headers }
-    withManager (httpLbs request)
-
-authHttpCall :: String -> String -> String -> RequestHeaders -> IO (Response LZ.ByteString)
-authHttpCall url user pass headers = do
-    r <- parseUrl url
-    let request = r {requestHeaders = headers }
-        requestWithAuth = applyBasicAuth (B.pack user) (B.pack pass) request
-    withManager (httpLbs requestWithAuth)
-
-httpGHEvents :: String -> String -> Maybe B.ByteString -> IO (Response LZ.ByteString)
-httpGHEvents user pass etag = authHttpCall  "https://api.github.com/events" user pass headers
-      where
-          headers = ("User-Agent","HTTP-Conduit"):maybe [] (\e -> [("If-None-Match",B.tail (B.tail e))]) etag
-
-showHeader :: Header -> IO ()
-showHeader (name, value) = B.putStrLn (original name <> ": " <> value)
-
-showHelpAndExit :: IO ()
-showHelpAndExit = do
-    putStrLn "provide your github username and password please"
-    exitFailure
+--------------------------------------------------------------------------------
+-- MAIN
+--------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-         [user,pass] -> getEvents user pass Nothing 100000 Nothing
+         [user,pass] -> getEvents user pass Nothing 100000 Nothing 1
          _ -> showHelpAndExit
+
+--------------------------------------------------------------------------------
+showHelpAndExit :: IO ()
+showHelpAndExit = do
+    putStrLn "provide your github username and password please"
+    exitFailure
+
+--------------------------------------------------------------------------------
+-- HTTP Calls
+--------------------------------------------------------------------------------
+
+authHttpCall :: String
+                -> String
+                -> String
+                -> RequestHeaders
+                -> [(B.ByteString,Maybe B.ByteString)]
+                -> IO (Response LZ.ByteString)
+authHttpCall url user pass headers params = do
+    r <- parseUrl url
+    let request = r {requestHeaders = headers}
+        requestWithParams = setQueryString params request
+        requestWithAuth = applyBasicAuth (B.pack user) (B.pack pass) requestWithParams
+    withManager (httpLbs requestWithAuth)
+
+httpGHEvents :: String
+                -> String
+                -> Maybe B.ByteString
+                -> Int
+                -> IO (Response LZ.ByteString)
+httpGHEvents user pass etag page =
+    authHttpCall "https://api.github.com/events" user pass headers params
+      where
+          headers = ("User-Agent","HTTP-Conduit"):
+                    maybe [] (\e -> [("If-None-Match",B.tail (B.tail e))]) etag
+          params = [("page",Just (B.pack (show page)))]
+
+--------------------------------------------------------------------------------
+-- Date
+--------------------------------------------------------------------------------
+
+showHeader :: Header -> IO ()
+showHeader (name, value) = B.putStrLn (original name <> ": " <> value)
 
 rfc822DateFormat :: String
 rfc822DateFormat = "%a, %_d %b %Y %H:%M:%S %Z"
@@ -62,12 +82,20 @@ rfc822DateFormat = "%a, %_d %b %Y %H:%M:%S %Z"
 epochFromString :: String -> Int
 epochFromString = floor . utcTimeToPOSIXSeconds . readTime defaultTimeLocale rfc822DateFormat
 
+--------------------------------------------------------------------------------
+-- Count how long a function evaluation took
+--------------------------------------------------------------------------------
+
 time :: IO a -> IO (Double, a)
 time action = do
     startTime <- getCPUTime
     res <- action
     endTime <- getCPUTime
     return (fromIntegral (endTime - startTime)/(10**12),res)
+
+--------------------------------------------------------------------------------
+-- JSON
+--------------------------------------------------------------------------------
 
 anArray :: Value -> Maybe Array
 anArray (Array a) = Just a
@@ -82,54 +110,75 @@ aString (String a) = Just a
 aString _ = Nothing
 
 getFirstId :: LZ.ByteString -> Maybe Text
-getFirstId body = decode body >>= anArray >>= (!? 0) >>= anObject >>= H.lookup "id" >>= aString
+getFirstId body = decode body >>= anArray >>= (!? 0) >>= anObject
+                    >>= H.lookup "id" >>= aString
+
+--------------------------------------------------------------------------------
+-- The Algorithm of getting events
+--------------------------------------------------------------------------------
+
+-- | Getting events
+getEvents :: String             -- ^ Github username
+          -> String             -- ^ Github password
+          -> Maybe B.ByteString -- ^ ETag
+          -> Int                -- ^ Time to wait in micro seconds
+          -> Maybe Text         -- ^ First Event Id
+          -> Int                -- ^ Page
+          -> IO ()
+getEvents user pass etag t oldFirstId page = do
+    -- Call /events on github
+    (req_time, response) <- time (httpGHEvents user pass etag page)
+    (timeToWaitIn_us,etagResponded,firstId,newPage) <-
+            getTimeAndEtagFromResponse t etag response req_time oldFirstId page
+    threadDelay timeToWaitIn_us
+    getEvents user pass etagResponded timeToWaitIn_us firstId newPage
+
+
+getTimeToWaitFromHeaders :: ResponseHeaders -> IO Int
+getTimeToWaitFromHeaders headers = do
+    -- Ask current date only if server didn't reponsded with its own
+    serverDateEpoch <- case lookup "Date" headers of
+                        Nothing -> fmap round getPOSIXTime
+                        Just d -> return (epochFromString (B.unpack d))
+    let remainingHeader = lookup "X-RateLimit-Remaining" headers
+        remaining = maybe 1 (read . B.unpack) remainingHeader
+        resetHeader = lookup "X-RateLimit-Reset" headers
+        reset = maybe 1 (read . B.unpack) resetHeader
+        timeBeforeReset = reset - serverDateEpoch
+    return (1000000 * timeBeforeReset `div` remaining)
+
+-- TODO: get the list of Ids from the body and check if firstId is in it
+containsId :: Maybe Text -> LZ.ByteString -> Bool
+containsId firstId body = True
 
 getTimeAndEtagFromResponse :: Int
                            -> Maybe B.ByteString
                            -> Response LZ.ByteString
                            -> Double
                            -> Maybe Text
-                           -> IO (Int, Maybe B.ByteString,Maybe Text)
-getTimeAndEtagFromResponse oldTime etag response req_time oldFirstId =
+                           -> Int
+                           -> IO (Int, Maybe B.ByteString,Maybe Text,Int)
+getTimeAndEtagFromResponse oldTime etag response req_time oldFirstId oldPage =
     if statusIsSuccessful (responseStatus response)
         then do
             let headers = responseHeaders response
-            -- If the server returned a date we use it
-            -- otherwise we use the local current time
-            serverDateEpoch <- case lookup "Date" headers of
-                                Nothing -> fmap round getPOSIXTime
-                                Just d -> return (epochFromString (B.unpack d))
+            t <- getTimeToWaitFromHeaders headers
             let etagResponded = lookup "ETag" headers
-                remainingHeader = lookup "X-RateLimit-Remaining" headers
-                remaining = maybe 1 (read . B.unpack) remainingHeader
-                resetHeader = lookup "X-RateLimit-Reset" headers
-                reset = maybe 1 (read . B.unpack) resetHeader
-                timeBeforeReset = reset - serverDateEpoch
-                t = 1000000 * timeBeforeReset `div` remaining
                 timeToWaitIn_us = max 0 (t - floor (1000000 * req_time))
+                firstId = getFirstId (responseBody response)
+                page = if containsId firstId (responseBody response) && (oldPage < 10)
+                          then oldPage + 1
+                          else 1
                 -- TODO: read all pages until we reach the first ID of the first page
                 -- of the preceeding loop
-                firstId = getFirstId (responseBody response)
+                -- return a new page if the first ID wasn't found
             publish (responseBody response) oldFirstId
-            return (timeToWaitIn_us,etagResponded,firstId)
+            return (timeToWaitIn_us,etagResponded,firstId,page)
         else do
             putStrLn (if notModified304 == responseStatus response
                         then "Nothing changed"
                         else "Something went wrong")
-            return (oldTime,etag,oldFirstId)
-
-getEvents :: String             -- ^ Github username
-          -> String             -- ^ Github password
-          -> Maybe B.ByteString -- ^ ETag
-          -> Int                -- ^ Time to wait in micro seconds
-          -> Maybe Text         -- ^ First Event Id
-          -> IO ()
-getEvents user pass etag t oldFirstId = do
-    -- Call /events on github
-    (req_time, response) <- time (httpGHEvents user pass etag)
-    (timeToWaitIn_us,etagResponded,firstId) <- getTimeAndEtagFromResponse t etag response req_time oldFirstId
-    threadDelay timeToWaitIn_us
-    getEvents user pass etagResponded timeToWaitIn_us firstId
+            return (oldTime,etag,oldFirstId,oldPage)
 
 -- takeUpUntil :: Maybe Text -> Maybe Value  -> Maybe [Value]
 -- takeUpUntil firstId = error "TODO"
